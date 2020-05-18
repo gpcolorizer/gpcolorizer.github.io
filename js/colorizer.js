c273135ed745db7aad1c8b3c4978a5b71d63d9d6
bt <btsmogon@gmail.com>
2020-05-17 20:06:26 -0400

    Do not overwrite blocks that are already colored

--- js/colorizer.js
+++ js/colorizer.js
@@ -6,37 +6,48 @@ function getRGBText(hex) {
     return `rgb(${r}, ${g}, ${b})`;
 }
 
+function regexEscape(s) {
+    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+}
+
+function insideOf(startSymbol, endSymbol, inclusive = false) {
+    [startSymbol, endSymbol] = [startSymbol, endSymbol].map(regexEscape);
+    let prefix = inclusive ? startSymbol : `(?<=${startSymbol})`;
+    let suffix = inclusive ? endSymbol : `(?=${endSymbol})`;
+    return new RegExp(`${prefix}(.*?)${suffix}`, 'gs');
+}
+
+function outsideOf(startSymbol, endSymbol) {
+    return new RegExp(`(?<=^|${regexEscape(endSymbol)})(.*?)(?=\$|${regexEscape(startSymbol)})`, 'gs');
+}
+
 function addPreamble(text) {
     return '[B]add[/B] [S]remove[/S] [B](comment)[/B]\n' + text;
 }
 
 function colorAdditions(text, color) {
-    let colorText = getRGBText(color);
-    function colorSingleBlock(_, p1, p2, p3) {
-        // Within bold blocks, color everything outside of parentheses
-        return p1 + p2.replace(/(^|\))(.+?)($|\()/gs, `$1[COLOR=${colorText}]$2[/COLOR]$3`) + p3;
-    }
-    return text.replace(/(\[B\])(.+?)(\[\/B\])/gs, colorSingleBlock);
+    let rgb = getRGBText(color);
+    return text.replace(insideOf('[B]', '[/B]'), boldText =>
+        boldText.replace(outsideOf('[COLOR=', '[/COLOR]'), nonColoredText =>
+            nonColoredText.replace(outsideOf('(', ')'), `[COLOR=${rgb}]$1[/COLOR]`)));
 }
 
 function colorRemovals(text, color, keepStrikethrough) {
-    let colorText = getRGBText(color);
+    let rgb = getRGBText(color);
     let replaceWith = keepStrikethrough
-        ? `[B][COLOR=${colorText}][S]$1[/S][/COLOR][/B]`
-        : `[B][COLOR=${colorText}]$1[/COLOR][/B]`;
-    return text.replace(/\[S\](.+?)\[\/S\]/gs, replaceWith);
+        ? `[B][COLOR=${rgb}][S]$1[/S][/COLOR][/B]`
+        : `[B][COLOR=${rgb}]$1[/COLOR][/B]`;
+    return text.replace(insideOf('[S]', '[/S]', true), replaceWith);
 }
 
 function colorComments(text, color, keepParens) {
-    let colorText = getRGBText(color);
-    function colorSingleBlock(_, p1, p2, p3) {
-        // Within bold blocks, color everything inside of parentheses
-        let replaceWith = keepParens
-            ? `[COLOR=${colorText}]($1)[/COLOR]`
-            : `[COLOR=${colorText}]$1[/COLOR]`;
-        return p1 + p2.replace(/\((.*?)\)/gs, replaceWith) + p3;
-    }
-    return text.replace(/(\[B\])(.+?)(\[\/B\])/gs, colorSingleBlock);
+    let rgb = getRGBText(color);
+    let replaceWith = keepParens
+        ? `[COLOR=${rgb}]($1)[/COLOR]`
+        : `[COLOR=${rgb}]$1[/COLOR]`;
+    return text.replace(insideOf('[B]', '[/B]'), boldText =>
+        boldText.replace(outsideOf('[COLOR=', '[/COLOR]'), nonColoredText =>
+            nonColoredText.replace(insideOf('(', ')', true), replaceWith)));
 }
 
 function colorize(text, options) {
